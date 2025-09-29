e905ba08337cde9c56b01c6d7abff987853dce7f
jeffrey-ho1 <ho2281@yahoo.com>
2025-09-29 14:53:56 +0200

    gemaakt opdarcht 4

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ extra_topping = input('Welke toppings wil je er nog meer bij hebben? ui/zalm/kip
 toppings.append(extra_topping)
 print(f"De laatse topping is nu {toppings[-1]}")
 print(f"Mijn pizza bevat nu de volgende toppings: {toppings}")
+remove_topping = input('Welke topping vind je  niet lekker? salami/kaas/tomaat/ananas/banaan/ui/zalm/kip')
+if remove_topping in toppings:
+        toppings.remove(remove_topping)
+        print(f"{remove_topping} is verwijderd uit de lijst")
+        print(f"De toppings zijn: {toppings}")
+else:
+        print(f"{remove_topping} zit niet in de lijst. De toppings zijn: {toppings}.")
+
+
+
