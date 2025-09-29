3fc3c2fe75e663fa7b7e533345e391c8c397afff
jeffrey-ho1 <ho2281@yahoo.com>
2025-09-29 13:18:31 +0200

    gemaakt opdracht 1 en 2

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 toppings: list[str] = ["salami","kaas","tomaat","ananas","banaan"]
 print(f"De eerste topping op mijn pizza is {toppings[0]}")
 print(f"De derde topping op mijn pizza is {toppings[2]}")
+print(f"De laatste topping op mijn pizza is {toppings[-1]}")
+print(f"Mijn pizza bevat de volgende toppings: {toppings}")
