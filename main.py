acda17f004fe3747ba6f015bc1ecff3d22f0e528
jeffrey-ho1 <ho2281@yahoo.com>
2025-09-29 13:13:56 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,3 @@
+toppings: list[str] = ["salami","kaas","tomaat","ananas","banaan"]
+print(f"De eerste topping op mijn pizza is {toppings[0]}")
+print(f"De derde topping op mijn pizza is {toppings[2]}")
